4ab37d4613ece386db5e747ab3b0b1d4adb0537d
sharkovic <sharkghost@mail.ru>
2020-01-24 17:28:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active'
+    }
+});
